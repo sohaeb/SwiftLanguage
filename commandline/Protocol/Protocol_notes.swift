d23f20b99ebae48464e60481761dbe07a117f8f1
Suhaib Abdulghani <snabieel@gmail.com>
2019-03-29 13:55:28 -0400

    Update Protocol_notes.swift

--- commandline/Protocol/Protocol_notes.swift
+++ commandline/Protocol/Protocol_notes.swift
@@ -8,14 +8,12 @@ Optional requirements are available so that you can write code that interoperate
 
 
 
+ //=====================================================================
+ //=====================================================================
+ //=====================================================================
 
 
-/*
- You can extend an existing type to adopt and conform to a new protocol,
- even if you don’t have access to the source code for the existing type.
- Extensions can add new properties, methods, and subscripts to an existing type,
- and are therefore able to add any requirements that a protocol may demand.
- */
+
  
  /*
  Conditionally Conforming to a Protocol
@@ -38,6 +36,11 @@ Optional requirements are available so that you can write code that interoperate
  print(myDice.textualDescription)
  // Prints "[A 6-sided dice, A 12-sided dice]"
  
+
+
+
+ //=====================================================================
+ //=====================================================================
  //=====================================================================
  
  /*
@@ -49,7 +52,11 @@ Optional requirements are available so that you can write code that interoperate
  extension Hamster: TextRepresentable {}
  
  //=====================================================================
+ //=====================================================================
+ //=====================================================================
  
+
+
  /*
  Collections of Protocol Types
  
@@ -75,5 +82,90 @@ Optional requirements are available so that you can write code that interoperate
  it’s safe to access thing.textualDescription each time through the loop.
  
  */
- 
+
+
+
+ //=====================================================================
+ //=====================================================================
+ //=====================================================================
+
+
+
+Protocol Extensions
+Protocols can be extended to provide method, initializer, subscript, and computed property implementations to conforming types. This allows you to define behavior on protocols themselves, rather than in each type’s individual conformance or in a global function.
+
+For example, the RandomNumberGenerator protocol can be extended to provide a randomBool() method, which uses the result of the required random() method to return a random Bool value:
+
+extension RandomNumberGenerator {
+    func randomBool() -> Bool {
+        return random() > 0.5
+    }
+}
+By creating an extension on the protocol, all conforming types automatically gain this method implementation without any additional modification.
+
+let generator = LinearCongruentialGenerator()
+print("Here's a random number: \(generator.random())")
+// Prints "Here's a random number: 0.3746499199817101"
+print("And here's a random Boolean: \(generator.randomBool())")
+// Prints "And here's a random Boolean: true"
+Protocol extensions can add implementations to conforming types but can’t make a protocol extend or inherit from another protocol. Protocol inheritance is always specified in the protocol declaration itself.
+
+
+
+
+ //=====================================================================
+ //=====================================================================
+ //=====================================================================
+
+
+
+
+
+Providing Default Implementations
+You can use protocol extensions to provide a default implementation to any method or computed property requirement of that protocol. If a conforming type provides its own implementation of a required method or property, that implementation will be used instead of the one provided by the extension.
+
+
+ //=====================================================================
+ //=====================================================================
+ //=====================================================================
+
+
+
+Adding Constraints to Protocol Extensions
+
+hen you define a protocol extension, you can specify constraints that conforming types must satisfy before the methods and properties of the extension are available. You write these constraints after the name of the protocol you’re extending by writing a generic where clause. For more about generic where clauses, see Generic Where Clauses.
+
+For example, you can define an extension to the Collection protocol that applies to any collection whose elements conform to the Equatable protocol. By constraining a collection’s elements to the Equatable protocol, a part of the standard library, you can use the == and != operators to check for equality and inequality between two elements.
+
+extension Collection where Element: Equatable {
+    func allEqual() -> Bool {
+        for element in self {
+            if element != self.first {
+                return false
+            }
+        }
+        return true
+    }
+}
+The allEqual() method returns true only if all the elements in the collection are equal.
+
+Consider two arrays of integers, one where all the elements are the same, and one where they aren’t:
+
+let equalNumbers = [100, 100, 100, 100, 100]
+let differentNumbers = [100, 100, 200, 100, 200]
+Because arrays conform to Collection and integers conform to Equatable, equalNumbers and differentNumbers can use the allEqual() method:
+
+print(equalNumbers.allEqual())
+// Prints "true"
+print(differentNumbers.allEqual())
+// Prints "false"
+NOTE
+
+If a conforming type satisfies the requirements for multiple constrained extensions that provide implementations for the same method or property, Swift uses the implementation corresponding to the most specialized constraints.
 */
+
+
+
+
+
+
